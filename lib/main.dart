233e66648389ddcd13af6b27db5956286de42e81
Vishalyadu1c <vishalyadav25062002@gmail.com>
2025-09-18 14:03:05 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tic Tac Toe Game',
+      title: 'Tic Tac Toe',
       debugShowCheckedModeBanner: false,
       home: const GameScreen(title: 'Tic-Tac-Toe'),
     );
